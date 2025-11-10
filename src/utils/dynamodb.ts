fe188a9de876fb44d8c57a93e82020b1cb1df5b9
kaye-dev <keien.nishikawa.developer@gmail.com>
2025-11-10 22:33:25 +0900

    DynamoDBの設定変更

--- src/utils/dynamodb.ts
+++ src/utils/dynamodb.ts
@@ -3,15 +3,6 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
   region: "ap-northeast-1",
-  ...(process.env.DYNAMODB_ENDPOINT && {
-    endpoint: process.env.DYNAMODB_ENDPOINT,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "dummy",
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "dummy",
-    },
-  }),
 });
-
 export const dynamoDbClient = DynamoDBDocumentClient.from(client);
-
-export const TABLE_NAME = "TodoTable";
+export const TABLE_NAME = process.env.DYNAMODB_TABLE_NAME;
